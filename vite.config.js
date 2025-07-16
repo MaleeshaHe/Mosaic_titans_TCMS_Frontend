935cfa12e06c35e051527cac4bdad7610e3b2916
Maleesha <maleesha@cypsolabs.com>
2025-07-17 02:06:27 +0530

    first commit

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import svgr from "vite-plugin-svgr";
+
+// https://vite.dev/config/
+export default defineConfig({
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://api.texascollege.me",
+        secure: false,
+      },
+    },
+  },
+  plugins: [react(), tailwindcss(), svgr()],
+});
